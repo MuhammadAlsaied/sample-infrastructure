3ccfa832d81bb5e3425962c723d8107070236bb2
Muhammad ALsaied <muhammadalsaied96@gmail.com>
2020-04-07 02:50:51 +0200

    add aws credentials to backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-backup-iti"
-    key    = "terraform.tfstate"
-    region = "eu-west-1"
+    bucket     = "terraform-state-backup-iti"
+    key        = "terraform.tfstate"
+    region     = "eu-west-1"
+    access_key = "${var.AWS_ACCESS_KEY_ID}"
+    secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
   }
 }
